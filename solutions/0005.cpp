b7b93d49ab3fe894d0c8033b94aef94c9ec7cbd8
BreeDurbin <sayamustfight@proton.me>
2023-12-12 18:38:43 -0600

    problem 0005 solution

--- solutions/0005.cpp
+++ solutions/0005.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return kaPow(x, (long long) n);
+    string longestPalindrome(string s) {
+        if(s.size() <= 1) return s;
+        string ans = "";
+        int n = s.size();
+        
+        for(int i=0; i<n; i++){
+            // odds
+            int dist = 0;
+            while(i - dist >= 0 && i + dist < n && s[i - dist] == s[i + dist]){
+                if(ans.size() < (2 * dist) + 1) ans = s.substr(i - dist, 2*dist + 1);
+                dist++;
+            }
+            // evens
+            int dist2 = 0;
+            while(i - dist2 >= 0 && i + dist2 + 1 < n && s[i - dist2] == s[i + dist2 + 1]){
+                if(ans.size() < 2 * (dist2 + 1)){
+                    ans = s.substr(i - dist2, 2*(dist2 + 1));
+                    cout << "HIYA:" << ans << endl;
+                }
+                dist2++;
+            }
+
+        }
+
+        return ans;
     }
-
-private:
-
-  double kaPow(double x, long long n){
-      if(n == 0) return 1.0;
-      if(n < 0) return 1.0 / kaPow(x, -1 * n);
-      if(n % 2 == 1) return x * kaPow(x*x, n / 2);
-      return kaPow(x*x, n / 2);
-  }
-
 };
