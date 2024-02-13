1a2f8f84044ae76133784efa138c21a885dea01a
BreeDurbin <sayamustfight@proton.me>
2024-02-12 19:10:08 -0600

    better solution 73

--- solutions/0073.cpp
+++ solutions/0073.cpp
@@ -1,23 +1,42 @@
 //time m*n
-//space m+n
+//space constant
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        vector<bool> rows(m), columns(n);
+        int m = matrix.size(), n = matrix[0].size(), row_0 = 1, col_0 = 1;
 
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j] == 0){
-                    rows[i] = true;
-                    columns[j] = true;
-                }
-            }
+            if(matrix[i][0] == 0) row_0 = 0;
+        }
+
+        for(int j=0; j<n; j++){
+            if(matrix[0][j] == 0) col_0 = 0;
         }
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(rows[i] == true || columns[j] == true) matrix[i][j] = 0;
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+        }
+
+        if(row_0 == 0){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+        if(col_0 == 0){
+            for(int j=0; j<n; j++){
+                matrix[0][j] = 0;
             }
         }
     }
