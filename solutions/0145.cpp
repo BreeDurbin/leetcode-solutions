d44a166f4459190e694b6e6d80ac3cf5366d6904
BreeDurbin <sayamustfight@proton.me>
2024-03-12 03:02:04 -0500

    Solution 0145

---
+++ solutions/0145.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ary;
+        postorderTraversal(root, ary);
+        return ary;
+    }
+private: 
+    void postorderTraversal(TreeNode* root, vector<int>& postorder) {
+        if(!root) return;
+        postorderTraversal(root->left, postorder);
+        postorderTraversal(root->right, postorder);
+        postorder.push_back(root->val);
+    }
+};
