c84cd3f8b1dab2e2f1ccbb6cdb3504888dcfbaf0
Bree Lynne Durbin <88691414+BreeDurbin@users.noreply.github.com>
2024-08-25 23:18:36 -0400

    Create 2089.cpp

---
+++ solutions/cpp/2089.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<int> ret;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == target) ret.push_back(i);
+        }
+        return ret;
+    }
+};
