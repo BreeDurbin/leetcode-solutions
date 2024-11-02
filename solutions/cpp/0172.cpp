1b435d31f6618be2b3f7a58cd372daa9079969b0
Bree Lynne Durbin <88691414+BreeDurbin@users.noreply.github.com>
2024-11-02 17:02:13 -0500

    Update 0172.cpp

--- solutions/cpp/0172.cpp
+++ solutions/cpp/0172.cpp
@@ -1,1 +1,17 @@
 
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+
+        for(int i = 5; i <= n; i++){
+            int cur = i;
+            while(cur % 5 == 0){
+                ans++;
+                cur /= 5;
+            }
+        }
+
+        return ans;
+    }
+};
