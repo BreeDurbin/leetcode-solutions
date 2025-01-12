285cd44d5103adf3d49c9d423c391824700c2109
Bree Lynne Durbin <88691414+BreeDurbin@users.noreply.github.com>
2025-01-12 12:52:34 -0600

    Update 0020.cpp

--- solutions/cpp/0020.cpp
+++ solutions/cpp/0020.cpp
@@ -1,28 +1,29 @@
 class Solution {
-    public:
-        bool isValid(string s) {
-
-            //if(s.size() == 1) return false;
-            //if(s[s.size() - 1] == '(' || s[s.size() - 1] == '{' || s[s.size() - 1] == '[') return false;
-            //if(s[0] == ')' || s[0] == '}' || s[0] == ']') return false;
-
-            std::stack<char> parens_stack;
-
-            for(int i = 0; i < s.size(); i++){
-                if(s[i] == '(' || s[i] == '{' || s[i] == '['){
-                    parens_stack.push(s[i]);
-                } else {
-                    if(parens_stack.empty() || parens_map[parens_stack.top()] != s[i]){
-                        return false;
-                    }
-                    parens_stack.pop();
-                }
+public:
+    bool isValid(string s) {
+        unordered_set<char> lefts({'(', '[', '{'});
+        stack<char> st;
+        int n = s.size();
+        
+        for(int i=0; i<n; i++){
+            if(lefts.count(s[i])){
+                st.push(s[i]);
+                continue;
             }
+            else if (!st.empty() && st.top() == '(' && s[i] == ')'){
+                st.pop();
+            }
+            else if (!st.empty() && st.top() == '[' && s[i] == ']'){
+                st.pop();
+            }
+            else if (!st.empty() && st.top() == '{' && s[i] == '}'){
+                st.pop();
+            }
+            else return false;
 
-            return parens_stack.empty();
         }
 
-    private:
-        unordered_map<char,char> parens_map = {{'(',')'}, {'{','}'}, {'[',']'}};
-
+        return st.empty();
+        
+    }
 };
