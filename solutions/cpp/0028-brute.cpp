87fba9a21d7df23fa90451e5e6334033a6d30f90
Bree Lynne Durbin <88691414+BreeDurbin@users.noreply.github.com>
2025-01-31 12:44:25 -0600

    Update and rename 0028.cpp to 0028-brute.cpp

--- solutions/cpp/0028-brute.cpp
+++ solutions/cpp/0028-brute.cpp
@@ -5,12 +5,9 @@ public:
         for(int i = 0; i < haystack.size(); i++){
             if(haystack[i] == needle[0]){
                 for(int samenessCount=1; samenessCount<=needle.size(); samenessCount++){
-                    std::cout << " i: " << i << " samenessCount: " << samenessCount << std::endl;
                     if(samenessCount == needle.size()){
-                        std::cout<< " returning int: " << i << std::endl;
                         return i;
                     } else if(haystack[i+samenessCount] != needle[samenessCount]){
-                        std::cout<< "breaking: " << haystack[i+samenessCount] << "!=" << needle[samenessCount] << std::endl;
                         samenessCount = needle.size() - 1;
                         break;
                     } 
