9ad37cf56b7d9a8c83661709fe174e0693c247bf
Bree Lynne Durbin <88691414+BreeDurbin@users.noreply.github.com>
2025-03-04 18:26:16 -0600

    Create 2579.cpp

---
+++ solutions/cpp/2579.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        if(n==1) return 1;
+        long long ans = 1;
+        for(int i=2; i<=n; i++) ans += 4*(i-1);
+        return ans;
+    }
+};
