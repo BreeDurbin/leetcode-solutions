8bf9ab9b9198d9d5fe0e180eab7fbdb4380ef63e
Bree Lynne Durbin <88691414+BreeDurbin@users.noreply.github.com>
2025-03-05 20:23:00 -0600

    Update 2965.cpp

--- solutions/cpp/2965.cpp
+++ solutions/cpp/2965.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    unordered_map<int,int> cache;
-    
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int repeated = 0, missing = 0, n = grid.size(), sum = 0, nSquared = n*n;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                cache[grid[i][j]]++;
-                if(cache[grid[i][j]] > 1){
-                    //cout << "found repeat " << endl;
-                    repeated = grid[i][j];
-                }
-                sum += grid[i][j];
-            }
-        }
+        unordered_map<int, int> f;
+        int m = grid.size(), n = grid[0].size();
         
-        int properSum = nSquared*(nSquared+1)/2;
-        missing = properSum - sum + repeated;
-        //cout << "n: " << n << " nSquared: " << nSquared << " sum: " << sum << " proper sum: " << properSum << endl;
-        return {repeated, missing};
+        for(auto row : grid) for(auto cell : row) f[cell]++;
+        
+        int a = -1, b = -1;
+        for(auto [key, val] : f) if(val > 1) a = key;
+        for(int i=1; i<=m*m; i++) if(f[i] == 0) b = i;
+        
+        return {a, b};
     }
 };
