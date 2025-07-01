4358dd4625f3a234e332b0e3c95ab0cdff4adbdf
Bree Lynne Durbin <88691414+BreeDurbin@users.noreply.github.com>
2025-06-30 23:48:18 -0500

    Update 3330.cpp

--- solutions/cpp/3330.cpp
+++ solutions/cpp/3330.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
-    int possibleStringCount(string word) {
-        int ans = 1, n = word.size();
-
-        for(int i=0; i<n;){
-            int j=0;
-            while(j<n){
-                if(j+1<n && word[j]==word[j+1]) ans += 1;
-                j++;
-            }
-            i=j;
-        }
-        
-
+    int possibleStringCount(string w) {
+        int n = w.size(), ans = w.size();
+        for(int i=1; i<n; i++) if(w[i] != w[i-1]) ans--;
         return ans;
     }
 };
