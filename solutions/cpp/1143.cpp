c5f871d60cfd2875b6b2e53d051f2a5f3f94b278
Bree Lynne Durbin <88691414+BreeDurbin@users.noreply.github.com>
2025-06-30 22:37:07 -0500

    Update 1143.cpp

--- solutions/cpp/1143.cpp
+++ solutions/cpp/1143.cpp
@@ -1,19 +1,17 @@
+
 class Solution {
 public:
-    int longestCommonSubsequence(string a, string b) {
-        if(a==b) return a.size();
-        vector<vector<int>> memo(a.size()+1, vector<int>(b.size()+1, 0));
-        for(int i=a.size()-1; i>=0;i--){
-            for(int j=b.size()-1; j>=0; j--){
-                if (a[i] == b[j]){
-                    memo[i][j] = 1 + memo[i+1][j+1];
-                }
-                else{
-                    memo[i][j] = max(memo[i+1][j], memo[i][j+1]);
-                }
+    int longestCommonSubsequence(string t1, string t2) {
+        int m = t1.size(), n = t2.size();
+
+        vector<vector<int>> lcs(m+1, vector<int>(n+1, 0));
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                lcs[i][j] = t1[i-1] == t2[j-1] ? 1 + lcs[i-1][j-1] : max(lcs[i-1][j], lcs[i][j-1]);
             }
         }
 
-        return memo[0][0];
+        return lcs[m][n];
     }
 };
