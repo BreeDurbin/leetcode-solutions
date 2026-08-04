849639ea691273682fa404afee092915fe9178df
Aphril <88691414+BreeDurbin@users.noreply.github.com>
2026-08-04 12:35:14 -0500

    Create 3731_2.cpp

---
+++ solutions/cpp/3731_2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        if(nums.size() <= 1) return {}; 
+        sort(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        for(int i = 1; i<nums.size(); i++){
+            for(int j = nums[i-1] + 1;  j < nums[i]; j++){
+                ans.push_back(j);
+            }
+        }
+
+        return ans;
+
+    }
+};
