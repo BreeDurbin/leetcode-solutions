f4a15bfe03009264c210d5f6ac5a0e41c5c45498
Aphril <88691414+BreeDurbin@users.noreply.github.com>
2026-09-02 15:27:58 -0500

    Create 3875.cpp

---
+++ solutions/cpp/3875.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+
+        // case 1: nums1 is all even or all odd, then return true;
+
+
+        // case 2: if nums1 has odds and evens then:
+        
+        // 2a: To construct all evens we need at least 2 odds
+        // to make an odd into an even we subtract an odd from it
+        
+        // case 2b: all odds, to make and even into an odd we subtract an odd from it
+        // so if there is at least 1 odd we can always make an all odd array
+
+        // so case 1 is always true and case 2 is always true so just return true.
+
+        return false;
+    }
+};
