486f0061fa1e1831e4efe61f490f765c29793603
Aphril <88691414+BreeDurbin@users.noreply.github.com>
2026-09-06 03:03:09 -0500

    Update 4043.cpp

--- solutions/cpp/4043.cpp
+++ solutions/cpp/4043.cpp
@@ -4,7 +4,7 @@ public:
         int number_of_rotations = s.size();
         int adj_pairs = 0;
 
-        for(int i=1; i<=s.size(); i++){
+        for(int i=1; i<s.size(); i++){
             if(s[i] == s[i-1]) adj_pairs++;
         }
         if(s[0] == s[s.size()-1]) adj_pairs++;
