5434892a15956e61e5a7d5b9a60d39123d1a2d54
Aphril <88691414+BreeDurbin@users.noreply.github.com>
2026-09-06 02:58:07 -0500

    Create 4043.cpp

---
+++ solutions/cpp/4043.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countRotations(string s, int k) {
+        int number_of_rotations = s.size();
+        int adj_pairs = 0;
+
+        for(int i=1; i<=s.size(); i++){
+            if(s[i] == s[i-1]) adj_pairs++;
+        }
+        if(s[0] == s[s.size()-1]) adj_pairs++;
+
+        if(k == adj_pairs) return number_of_rotations - adj_pairs;
+        if(k == adj_pairs - 1) return adj_pairs;
+
+        return 0;
+    }
+
+};
